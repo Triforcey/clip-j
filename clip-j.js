e758144cb71b97602d1eec77c95fbea1722647c7
Triforcey <spaceshipfarmer@gmail.com>
2015-09-11 16:57:52 -0600

    fixed bugs

--- clip-j.js
+++ clip-j.js
@@ -20,10 +20,12 @@ function clipImage(imgsrc) {
 	var canvas = document.createElement('canvas');
 	var img = document.createElement('img');
 	img.setAttribute('src', imgsrc);
+	document.body.appendChild(img);
 	canvas.width = img.clientWidth;
 	canvas.height = img.clientHeight;
 	var idc = canvas.getContext('2d');
 	idc.drawImage(imgsrc, 0, 0);
 	var imgd = canvas.toDataURL();
+	img.remove();
 	clip(imgd);
 }
