25f72b44a66af50fa1c0e02d5ed75c1a3d5fc494
Triforcey <kd7yva+triforcey@gmail.com>
2015-12-19 21:37:09 -0700

    Bug fixing.

--- clip-j.js
+++ clip-j.js
@@ -5,8 +5,8 @@ function clip(text) {
 	copyElement = document.body.appendChild(copyElement);
 	copyElement.select();
 	try {
-		document.execCommand('copy');
-	} catch (e) {
+		if(!document.execCommand('copy')); throw 'Not allowed.';
+	} catch(e) {
 		copyElement.remove();
 		console.log("document.execCommand('copy'); is not supported");
 		prompt('Copy the text below. (ctrl c, enter)', text);
